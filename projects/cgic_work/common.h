4c7a6f18f06390edd1e4c17f88a2aed8303dfb5d
emiliano bergamaschini <ebergamaschini@mect.it>
2016-03-25 15:04:17 +0000

    remove warnings and update the makefile for the new cgi

--- projects/cgic_work/common.h
+++ projects/cgic_work/common.h
@@ -1,12 +1,10 @@
 #define SERVER_NAME "www.tpac010.net"
 
-extern int HandleSubmitALARMTBL(void);
 extern int HandleSubmitCROSSTBL(void);
-extern int HandleSubmitCFGTBL(void);
-extern int HandleSubmitATNCFG(void);
 extern int HandleSubmitSPLASH(void);
 extern int HandleSubmitHMI(void);
 extern int HandleSubmitFCRTS(void);
 extern int HandleSubmitPLC(void);
 extern int HandleSubmitSYSUPD(void);
+extern int HandleSubmitSYSINI(void);
 

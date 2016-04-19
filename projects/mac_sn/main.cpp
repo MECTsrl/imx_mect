56f62a508d0f2faa9ae66f7e677b4778b6013d3c
emiliano bergamaschini <ebergamaschini@mect.it>
2016-04-19 08:39:50 +0000

    add vnc option to SDcard and mac_sn

--- projects/mac_sn/main.cpp
+++ projects/mac_sn/main.cpp
@@ -20,7 +20,30 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    QApplication a(argc, argv);
+    int width = 1480;
+    int height = 1272;
+    int rotation = 0;
+    char vncString[256];
+
+    FILE * fp = fopen ("/sys/class/graphics/fb0/virtual_size", "r");
+    if (fp)
+    {
+        fscanf(fp, "%d,%d", &width, &height);
+        fclose(fp);
+    }
+
+    sprintf(vncString, "Multi: VNC:0:size=%dx%d Transformed:rot%d", width, height, rotation);
+
+    int myargc = argc + 4;
+    char *myargv[] =
+    {
+        argv[0],
+        strdup("-qws"),
+        strdup("-display"),
+        vncString
+    };
+
+    QApplication a(myargc, myargv);
 
     /* set the cursor as invisible */
     QWSServer::setCursorVisible(false);

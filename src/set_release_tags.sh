ac734d7bed0cc0d1f038eb44e6fd075a9b3f9d7a
Mihai Lazarescu <mlazarescu@mect.it>
2016-11-15 17:04:27 +0100

    Better select version setup spots in Makefile.

--- src/set_release_tags.sh
+++ src/set_release_tags.sh
@@ -6,13 +6,13 @@ MECT_PLUGINS=$(git ls-remote --tags https://github.com/MECTsrl/mect_plugins.git 
 ATCMCONTROLRUNTIMESYSTEM=$(git ls-remote --tags https://github.com/MECTsrl/ATCMcontrol_RunTimeSystem.git | sort -t/ -k3 -V | cut -d/ -f3 | tail -n 1)
 
 echo "Setting MECT_BUILD_IMXMECT_TAG to $IMX_MECT"
-sed -i '/\bMECT_BUILD_IMXMECT_TAG\b/ s/\(MECT_BUILD_IMXMECT_TAG.*=\).*/\1 '$IMX_MECT'/' Makefile
+sed -i '/^\(export \)\?\bMECT_BUILD_IMXMECT_TAG\b/ s/\(MECT_BUILD_IMXMECT_TAG.*=\).*/\1 '$IMX_MECT'/' Makefile
 
 echo "Setting MECT_BUILD_ATCMCRT_TAG to $ATCMCONTROLRUNTIMESYSTEM"
-sed -i '/\bMECT_BUILD_ATCMCRT_TAG\b/ s/\(MECT_BUILD_ATCMCRT_TAG.*=\).*/\1 '$ATCMCONTROLRUNTIMESYSTEM'/' Makefile
+sed -i '/^\(export \)\?\bMECT_BUILD_ATCMCRT_TAG\b/ s/\(MECT_BUILD_ATCMCRT_TAG.*=\).*/\1 '$ATCMCONTROLRUNTIMESYSTEM'/' Makefile
 
 echo "Setting MECT_BUILD_PLUGINSCRT_TAG to $MECT_PLUGINS"
-sed -i '/\bMECT_BUILD_PLUGINSCRT_TAG\b/ s/\(MECT_BUILD_PLUGINSCRT_TAG.*=\).*/\1 '$MECT_PLUGINS'/' Makefile
+sed -i '/^\(export \)\?\bMECT_BUILD_PLUGINSCRT_TAG\b/ s/\(MECT_BUILD_PLUGINSCRT_TAG.*=\).*/\1 '$MECT_PLUGINS'/' Makefile
 
 echo "Setting MECT_BUILD_APPSCRT_TAG to $MECT_APPS"
-sed -i '/\bMECT_BUILD_APPSCRT_TAG\b/ s/\(MECT_BUILD_APPSCRT_TAG.*=\).*/\1 '$MECT_APPS'/' Makefile
+sed -i '/^\(export \)\?\bMECT_BUILD_APPSCRT_TAG\b/ s/\(MECT_BUILD_APPSCRT_TAG.*=\).*/\1 '$MECT_APPS'/' Makefile

d4796a3f36f91f0ad31877b4357f4a1094bfee89
Mihai Lazarescu <mlazarescu@mect.it>
2017-07-24 07:44:31 +0000

    Fail sysupdate VPN if no certificate.

--- src/sysupdate_vpn_post.sh
+++ src/sysupdate_vpn_post.sh
@@ -4,44 +4,44 @@ if test -f "$LUP"; then
     chmod 0600 "$LUP"
 fi
 
-# Add/replace the new OpenVPN certificate.
-if test -r "${MNTDIR}/${SN}.ovpn" -a "$(sed -n '/^\s*Subject: .*CN=/ { s/^.*CN=//; s/\/.*//; s/-mect$//; p; }' ${MNTDIR}/${SN}.ovpn)" = "$SN"; then
-    if ! test -d "$OVPNCONF"; then
-	rm -f "$OVPNCONF"
-	mkdir -p "$OVPNCONF"
-    fi
+# Any OpenVPN certificate for this device?
+test -r "${MNTDIR}/${SN}.ovpn" -a "$(sed -n '/^\s*Subject: .*CN=/ { s/^.*CN=//; s/\/.*//; s/-mect$//; p; }' ${MNTDIR}/${SN}.ovpn)" = "$SN" || do_exit "no OpenVPN certificate for this device." | tee /dev/tty1
 
-    if test -d "$OVPNCONF"; then
-	# Disable any existing certificates.
-	find "$OVPNCONF" -type f \( -iname \*.ovpn -o -iname \*.conf \) -exec mv {} {}.$(date '+%F-%T') \;
-
-	install -m 644 "${MNTDIR}/${SN}.ovpn" "${OVPNCONF}/${SN}.ovpn"
-	test -s "${OVPNCONF}/${SN}.ovpn" || echo -e "${ERRMSG} cannot install the OpenVPN certificate.\r" | tee /dev/tty1
-    else
-	echo -e "${ERRMSG} no OpenVPN configuration directory.\r" | tee /dev/tty1
-    fi
-else
-    echo -e "${ERRMSG} no OpenVPN certificate for this device.\r" | tee /dev/tty1
+# Make sure that OpenVPN configuration directory exists.
+if ! test -d "$OVPNCONF"; then
+    rm -f "$OVPNCONF"
+    mkdir -p "$OVPNCONF"
 fi
+test -d "$OVPNCONF" || do_exit "no OpenVPN configuration directory." | tee /dev/tty1
+
+# Add/replace the new OpenVPN certificate.
+#
+
+# Disable any existing certificates.
+find "$OVPNCONF" -type f \( -iname \*.ovpn -o -iname \*.conf \) -exec mv {} {}.$(date '+%F-%T') \;
+
+# Install the new certificate.
+install -m 644 "${MNTDIR}/${SN}.ovpn" "${OVPNCONF}/${SN}.ovpn"
+test -s "${OVPNCONF}/${SN}.ovpn" || do_exit "cannot install the OpenVPN certificate." | tee /dev/tty1
+
+# Change the default (development) root password.
+sed -i 's:^root\:[^\:]\+\::root\:$1$5FNVDbNV$Qkv/sIFbIQwuZdbz6rdIs0\::' "$SHADOWFILE"
 
 # Make sure that the cron tab is set for periodic log upload.
 test -s "$CRONTAB" || touch "$CRONTAB"
-
-# Uncomment the sync schedule, if present.
+# Uncomment the sync schedule in cron tab, if commented.
 sed -i "/ $(echo $LUS | sed 's|/|\\/|g')"'\s*$/ { s/^[ 	#]*//; s/\s*$//; }' "$CRONTAB"
-
-# Still no schedule?  Add it!
+# Still no sync schedule?  Add it!
 if ! grep -q " $LUS\\s*\$" "$CRONTAB"; then
     sed -n "/ $(echo $LUS | sed 's|/|\\/|g')"'\s*$/ { s/^[ 	#]*//; s/\s*$//; p; }' "${CRONTAB}.default" >> "$CRONTAB"
-
-    # Still no schedule by now?  That's an error.
-    grep -q "$LUS\\s*\$" "$CRONTAB" || echo -e "${ERRMSG} cron tab setup failed.\r" | tee /dev/tty1
+    # Still no schedule by now?  That's very weird...
+    grep -q "$LUS\\s*\$" "$CRONTAB" || do_exit "cron tab setup failed." | tee /dev/tty1
 fi
 
-# Activate configuration changes.
+# Activate the configuration changes.
 "$OVPNRC" stop; sleep 1; "$OVPNRC" start
 "$CRONRC" stop; sleep 1; "$CRONRC" start
 
-# Exit
+# All set.
 ( echo -e "\r"; echo -e "Device update for MECT Remote Services succeeded.\r" ) | tee /dev/tty1
 do_exit

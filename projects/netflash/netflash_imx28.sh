466f760f9cb4264c1d41dfef421ff42d1fda4116
Mihai Lazarescu <mlazarescu@mect.it>
2017-11-11 01:59:22 +0000

    Speed improvment netflash.sh

--- projects/netflash/netflash_imx28.sh
+++ projects/netflash/netflash_imx28.sh
@@ -109,7 +109,8 @@ sudo sshpass -p "$passwd" rsync -axh --inplace --delete --info=progress2 -e "ssh
 sudo sshpass -p "$passwd" rsync -axh --delete -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" rootfs/ "$user"@"$ip":/
 
 echo "* Update the local file system"
-sudo sshpass -p "$passwd" rsync -axh --delete --info=progress2 -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" rootfs/local/ "$user"@"$ip":/local/
+sudo sshpass -p "$passwd" rsync -axh --inplace --delete --info=progress2 -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" rootfs/local/ "$user"@"$ip":/local/
+sudo sshpass -p "$passwd" rsync -axh --delete -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" rootfs/local/ "$user"@"$ip":/local/
 
 echo "* Closing and rebooting target..."
 sshpass -p "$passwd" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@"$ip" /bin/sync\; /sbin/reboot
